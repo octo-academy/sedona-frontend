c99b6e28fd1cc32013c7824011e105f8f421dcbc
Julia Volkova <julia.volkova789@gmail.com>
2018-04-30 20:06:27 +0300

    chore: Add basic project setup

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        compress: true
+    },
+    module: {
+        rules: [
+            { test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }, {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    { loader: 'file-loader' }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
+};
