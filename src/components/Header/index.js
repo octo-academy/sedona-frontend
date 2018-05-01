5f312ec9f1a5effaf94c5c426a57a97c7e986d24
Julia Volkova <julia.volkova789@gmail.com>
2018-05-01 19:31:49 +0300

    chore: Add new dependencies

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Header.css';
+
+export const Header = () => (
+    <nav>
+        <ul>
+            <li>Информация</li>
+            <li>Фото и видео</li>
+            <li>Карта штата</li>
+            <li>Гостиницы</li>
+        </ul>
+    </nav>
+);
