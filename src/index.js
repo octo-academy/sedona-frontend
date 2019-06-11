9c969fb925b5c485f255f9ad6f98ad97d546d9e9
Denis Verkhoturov <mod.satyr@gmail.com>
2019-06-11 21:10:18 +0300

    chore & dev: Initialize simple Saga and all the required configurations

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import {applyMiddleware, compose, createStore, dispatch} from 'redux';
+import {applyMiddleware, compose, createStore, dispatch} from 'redux';
 import { AppContainer } from 'react-hot-loader';
-// import createSagaMiddleware from 'redux-saga';
-// import logger from 'redux-logger'
-// import './style.css';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger'
 import { App } from './components/App';
-// import combineReducer from './reducers';
-// import rootSaga from "./saga";
+import combineReducer from './reducers';
+import rootSaga from "./sagas";
 
-// const initialState = {};
+const initialState = {};
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const sagaMiddleware = createSagaMiddleware();
-// const middleware = applyMiddleware(sagaMiddleware, logger);
+const sagaMiddleware = createSagaMiddleware();
+const middleware = applyMiddleware(sagaMiddleware, logger);
 
-// export const store = createStore(combineReducer, initialState, composeEnhancers(middleware));
-// sagaMiddleware.run(rootSaga);
+const store = createStore(combineReducer, initialState, middleware);
+sagaMiddleware.run(rootSaga);
 
-const render = Component => {
-    ReactDOM.render(
-        <Provider store={{}}>
-            <AppContainer>
-                <Component/>
-            </AppContainer>
-        </Provider>,
-        document.getElementById('root'),
-    )
-};
+const render = Component => ReactDOM.render(
+    <Provider store={{}}>
+        <AppContainer>
+            <Component/>
+        </AppContainer>
+    </Provider>,
+    document.getElementById('root'),
+);
 
 render(() => <App />);
